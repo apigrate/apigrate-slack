d6b8bc614569453b4fc39f63469691dec93e428e
gaumeister <derek@apigrate.com>
2018-01-11 10:47:51 -0600

    Standardized logging conventions. Added Built.io custom action code.

--- index.js
+++ index.js
@@ -1,191 +1,143 @@
-//v1.0.1
-var _ = _rqr('lodash');
-var moment = _rqr('moment');
-var request = _rqr('request');
-// -----------------------------------------------------------------------------
-/* for nodejs + builtio */
-function _rqr(lib){ return typeof $require != 'undefined' ? $require(lib) : require(lib); }
-function _exp(constr){ if(typeof $export != 'undefined'){ $export(null, constr); } else { module.exports=constr; } }
-// -----------------------------------------------------------------------------
+var _ = require('lodash');
+var moment = require('moment');
+var request = require('request');
 
 /**
-  A Convenience class for working with Slack Inbound webhooks. It is primarily intended for
-  logging use. You can provide attachments on the methods or not. If you do, the timestamp
-  will be generated automatically for you. You can also provide defaults for the attachments
-  as options in the constructor. These defaults will only be used when others have not been provided.
+  An Apigrate-specific logging tool for working with Slack Inbound webhooks. It is intended for
+  standardizing success/failure reporting on app solutions.
 
-  @param {string} endpoint the inbound webhook to use.
-  @param {object} options other options that are defaults to be used on attachments
-  @example showing the defaults
+
+    By convention, these colors are used for attachment content on error, warn, ok and
+    question messages respectively.
+    success=false: danger,
+    success=true: good
+
+    By convention, these emojis are used for consistency
+    question messages respectively.
+    success=false: 	:x:
+    success=true: :white_check_mark:
+
+  @param {string} inbound_webhook the inbound webhook to use.
+  @param {string} hostname where the solution is hosted. If on a middleware platform, the name of that platform. If on an app server, the symbolic name of the
+  server for ease of identification.
+  @param {string} solution the solution (identifiable app name producing this log, required for reporting purposes.
+  @param {object} options other options
+  @example
   {
-    author_name: 'string',// The author name or source of the log, typically an app or a flow name
-    author_link: 'string', // The author_name URL link. You might make this a flow URL for example
-    author_icon: 'string', // URL to a icon for the author_name
-    title: 'string',// The title
-    title_link: 'string', // The url for the title
-    fields: [], //Fields to add to each attachment
-    measureTiming: false, // Log request timing to console?
-    markdownAttachments: true //Whether to enable markdown in attachment text and pretext fields
+    solution_url: 'string', // The optional solution URL link. Could be README documentation, for example for an app.
   }
 
-  By convention, these colors are used for attachment content on error, warn, ok and
-  question messages respectively.
-  error: danger,
-  warn: warning,
-  ok: good,
-  question: (none)
+  @version 2.0.0
 
-  By convention, these emojis are used for consistency on error, warn, ok and
-  question messages respectively.
-  Error: 	:skull_and_crossbones:
-  Warn: :warning:
-  OK: :white_check_mark:
-  Question: :question:
 */
-function SlackPoster(endpoint, options){
-  this.options = options;
-  if(!options){
+function SlackLogger(inbound_webhook, hostname, solution, options) {
+  if( _.isNil(inbound_webhook) || _.isNil(hostname) || _.isNil(solution) ){
+    throw new Error("Misconfigured SlackLogger. The inbound_webhook, hostname, and solution parameters are all required.");
+  }
+  this.inbound_webhook = inbound_webhook;
+  this.hostname = hostname;
+  this.solution = solution;
+  if (!options) {
     this.options = {};
   }
-  this.baseRequest = request.defaults({
-    baseUrl: endpoint,
-    headers:{
-      'Accept': 'application/json'
-    },
-    time: this.options.measureTiming || false
-  });
-  if(!options.markdownAttachments){
-    options.markdownAttachments=true;
-  }
-}
-
-SlackPoster.prototype.error = function(message, attachment){
-  var self = this;
-  message = ':skull_and_crossbones:' + message;
-  _setAttachment(attachment, 'color', 'danger');
-  return self.post(message, attachment);
-}
-SlackPoster.prototype.warn = function(message, attachment){
-  var self = this;
-  message = ':warning:' + message;
-  _setAttachment(attachment, 'color', 'warning');
-  return self.post(message, attachment);
-}
-SlackPoster.prototype.ok = function(message, attachment){
-  var self = this;
-  message = ':white_check_mark:' + message;
-  _setAttachment(attachment, 'color', 'good');
-  return self.post(message, attachment);
-}
-SlackPoster.prototype.question = function(message, attachment){
-  var self = this;
-  message = ':question:' + message;
-  return self.post(message, attachment);
-}
-SlackPoster.prototype._setDefaults = function(attachment){
-  var self = this;
-  if(!_.isNil(attachment)){
-
-
-    if(!_.isNil(self.options.author_name)){
-      _setAttachment(attachment, 'author_name', self.options.author_name);
-    }
-    if(!_.isNil(self.options.author_link)){
-      _setAttachment(attachment, 'author_link', self.options.author_link);
-    }
-    if(!_.isNil(self.options.author_icon)){
-      _setAttachment(attachment, 'author_icon', self.options.author_icon);
-    }
-    if(!_.isNil(self.options.title)){
-      _setAttachment(attachment, 'title', self.options.title);
-    }
-    if(!_.isNil(self.options.title_link)){
-      _setAttachment(attachment, 'title_link', self.options.title_link);
-    }
-
-    if(_.isArray(attachment)){
-      if(!_.isNil(self.options.fields)){
-        _.each(attachment, function(a){
-          a.fields = _.concat(a.fields, self.options.fields);
-          if(self.options.markdownAttachments){
-            a.mrkdwn_in=["text","pretext"];
-          }
-        });
-      }
-    } else {
-      if(!_.isNil(self.options.fields)){
-        attachment.fields = _.concat(attachment.fields, self.options.fields);
-      }
-      if(self.options.markdownAttachments){
-        attachment.mrkdwn_in=["text","pretext"];
-      }
-    }
-
-  }
-}
-function _setAttachment(attachment, attribute, value){
-  if(!_.isNil(attachment)){
-    if(_.isArray(attachment)){
-      _.each(attachment, function(a){
-        var existing = a[attribute];
-        if(_.isNil(existing)||existing===''){
-          a[attribute] = value;
-        }
-      });
-    } else {
-      var existing = attachment[attribute];
-      if(_.isNil(existing)||existing===''){
-        attachment[attribute] = value;
-      }
-    }
-  }
-
 }
 
 /**
-  @param {string} message the main message
-  @param {object|array} attachment a single attachment object (see Slack documentation)
-  or an array of attachments.
+  Log a summary message.
+
+  @param {boolean} success required true/false
+  @param {string} entity required type of the primary entity for the solution ("what kind of thing is being worked on")
+  @param {string} entity_id required identifier for the primary entity.
+  @param {string} summary required a short summary message (i.e. "synced ok", "error processing account", etc.)
+  @param {string} details optional details to be displayed in fixed-width block under the message. Use this to output transcript info.
+  Up to 7500 characters will be written, after which the data will be truncated.
 */
-SlackPoster.prototype.post = function(message, attachment){
+SlackLogger.prototype.log = function(success, entity, entity_id, summary, details) {
   var self = this;
-  var payload = {
-    "text": message
+
+  if( _.isNil(success) || _.isNil(entity) || _.isNil(entity_id) || _.isNil(summary) ){
+    throw new Error("Invalid log invocation. The success, entity, entity_id, and summary parameters are all required.");
   }
-  if(!_.isNil(attachment)){
-    self._setDefaults(attachment);
-    //attach timestamp
-    var timestamp = moment().format('X');
-    _setAttachment(attachment, 'ts', timestamp);
-    if(_.isArray(attachment)){
-      payload.attachments = attachment;
+
+  var color = "good";
+  var emoji = ":white_check_mark:";
+  if (success) {
+    color = "good";
+    emoji = ":white_check_mark:";
+  } else {
+    color = "danger";
+    emoji = ":x:";
+  }
+
+  var title = emoji + ' ' + summary;
+
+  var text = "";
+  if (details) {
+    if (details.length > 7500) {
+      text = details.substr(0, 7500) + "...";
     } else {
-      payload.attachments = [ attachment ];
+      text = details;
     }
   }
 
+  if (text && text.trim().length > 0) {
+    text = "```" + text + "```";
+  }
 
-  self.baseRequest.post({uri:'', body: payload, json: true}, function(err, resp, body){
+  var slack_message = {
+    "username": self.hostname,
+    "attachments": [
+      {
+        "color": color,
+        "author_name": self.solution || "",
+        "author_link": self.options.solution_url || "",
+        "title": title,
+        "text": text,
+        "ts": moment().unix(),
+        "mrkdwn_in": ["text"],
+        "fields": [
+          {
+            "title": "solution",
+            "value": self.solution,
+            "short": true
+          }, {
+            "title": "success",
+            "value": success
+              ? "true"
+              : "false",
+            "short": true
+          }, {
+            "title": "entity",
+            "value": entity,
+            "short": true
+          }, {
+            "title": "entity id",
+            "value": entity_id,
+            "short": true
+          }
 
-    if(self.options.measureTiming){
-      if(resp.timingPhases){
-        resp.timingPhases.timingStart = resp.timingStart;
-        console.log( " TIMING "+ resp.timingStart +" (GET "+objectName+" BY ID): " + JSON.stringify(resp.timingPhases) );
-      } else if (resp.elapsedTime) {
-        console.log( " TIMING (GET "+objectName+" BY ID): " + resp.elapsedTime + ' elapsed.');
+        ]
+      }
+    ]
+  };
+
+  return request({
+    method: "POST",
+    url: self.inbound_webhook,
+    json: true,
+    body: slack_message
+  }, function(err, resp, body) {
+    if (err) {
+      throw err;
+    } else {
+      if (body == 'ok') {
+        return {success: true};
+      } else {
+        throw new Error("Error. Slack responded with: " + body);
       }
     }
 
-    if(err){
-      console.error(err.message);
-    }
-
-    console.log(body);
-
   });
+};
 
-
-}
-
-
-_exp(SlackPoster);
+module.exports=SlackLogger;
