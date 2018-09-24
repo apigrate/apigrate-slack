b6fb33d6f3ddc6dd869e0ddf167758b878d8b495
gaumeister <derek@apigrate.com>
2018-09-24 10:18:29 -0500

    v3.0.0. Native promises, more flexible field options.

--- README.md
+++ README.md
@@ -1,49 +1,82 @@
 # apigrate-slack
 A simple utility to post messages to Slack using inbound webhooks.
 
+Version 3.0.0 contains breaking changes:
+1. `hostname` in the constructor has been renamed to `username`. Semantically it still means the same thing.
+1. the `entity` and `entity_id` parameters are no longer part of the logging message. Use the fields hash instead.
+
 ## Usage
 
-Instantiating...
-```
-var webhook = 'https://hooks.slack.com/services/TTTTTTTTT/BBBBBBBBBB/XXXXXXXXXXXXXXXXX';
+### Instantiation
+```javascript
+var webhook = 'your inbound webhook here';
 
-var SLOGGER = new SlackLogger(webhook,
-  "localhost",
+var slack_logger = new SlackLogger(webhook,
+  "test environment",
   "SlackLogger Test"
 );
 ```
-In the above example, the app name  for each slack entry will be 'localhost' and the username will be "SlackLogger Test".  
+In the above simple example, the username for **each slack entry** will be 'test environment' and the author will be "SlackLogger Test". By convention, we use username to store our environment or host name and use the author to identify the app making the logging call from that environment. You are, of course, free to implement your own conventions.
 
-Each log message must have the following parameters:
+Here is a more detailed instantiation that includes additional information to be recorded on every logging call.
+```javascript
+var webhook = 'your inbound webhook here';
+
+var slack_logger = new SlackLogger(webhook,
+  "test environment",
+  "Invoice Integration",
+  {
+    author_url: "http://documentation.about.invoice.integration",
+    fields: {
+      account_id: 24601,
+      environment: "test"
+    }
+  }
+);
+```
+In this case, `account_id` and `environment` fields will be added to the slack message on every log entry. The "Invoice Integration" author will be hyperlinked as well with the value of `author_url`. This is a nice way to tie documentation together with your log entries, or to hyperlink to an app that is logging activity.
+
+### Logging
+Each log message **must** have the following parameters:
 
 1. true/false indicating success or failure
-1. the name of the primary entity involved in the transaction
-1. the id of the primary entity involved in the transaction
-1. the log message
+1. the log message (i.e. a summary)
 1. a more detailed message (optional). This could be log details, stack trace or other detailed information for the message (typically more useful to provide troubleshooting info on errors).
 
-### Examples
-Here's how to post a "success" message.
-```
-//parms: success, entity name, entity id, message, detailed message
-SLOGGER.log(
+
+Here's how to post a simple "success" message.
+```javascript
+slack_logger.log(
   true,
-  'Account',
-  4,
-  'Success! Everything is OK.',
-  null
+  'test environment',
+  'Success! Everything is OK.'
 );
 ```
 
-Here's how to post an error message with some detailed logging information.
+It is possible include more detail in log messages. The fields hash parameter allows you to specify additional data that may be helpful for reporting or troubleshooting **on specific log messages**. Note that the the logging details will be formatted in fixed-font "code block" automatically for readability. Newline characters are respected by Slack in the formatting.
+```javascript
+slack_logger.log(
+  true,
+  'Invoice Created.',
+  'Found customer.\nThere are 3 invoice lines.\nThe total amount is $107.80',
+  {
+    customer_id: 28390,
+    invoice_id: 123789
+  }
+);
 ```
-SLOGGER.log(
+
+Error log messages (as shown below) are more useful when they include detailed information to allow consumers to troubleshoot more effectively.
+```javascript
+slack_logger.log(
   false,
-  'Account',
-  4,
-  'Error processing Account.',
-  'Unable to connect to the database. There may be a network problem. Please contact your network administrator.'
+  'Invoice Created.',
+  'Found customer.\nException processing invoice lines.\nThe quantity is missing for the line with product 1234879.',
+  {
+    customer_id: 28390,
+    product_id: 1234879,
+    product_sku: 'TS4921'
+  }
 );
 
 ```
-...Note that the the logging details will be formatted in a markdown "code block" automatically for readability. You do not need to add these backticks yourself.
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var _ = require('lodash');
 var moment = require('moment');
-var request = require('request');
+var request = require('request-promise-native');
 
 /**
-  An Apigrate-specific logging tool for working with Slack Inbound webhooks. It is intended for
-  standardizing success/failure reporting on app solutions.
-
+  A logging tool for working with Slack Inbound webhooks. It is intended for logging
+  application automation transactions in Slack, providing transparency about how
+  automation solutions are performing.
 
     By convention, these colors are used for attachment content on error, warn, ok and
     question messages respectively.
@@ -18,142 +18,152 @@ var request = require('request');
     success=true: :white_check_mark:
 
   @param {string} inbound_webhook the inbound webhook to use.
-  @param {string} hostname where the solution is hosted. If on a middleware platform, the name of that platform. If on an app server, the symbolic name of the
+  @param {string} username where the author is hosted. If on a middleware platform, the name of that platform. If on an app server, the symbolic name of the
   server for ease of identification.
-  @param {string} solution the solution (identifiable app name producing this log, required for reporting purposes.
-  @param {object} options other options
+  @param {string} author name of the author (identifiable app name producing this log, required for reporting purposes.
+  @param {object} there are two supported options:
+  1. "author_url" This is an optional string representing the author URL link. Could be README documentation, for example for an app.
+  2. "fields" a hash of properties that will be added as short fields on every slack entry produced by this logger.
   @example
   {
-    solution_url: "https://www.example.com/myapp/readme",
-    customer_id: 'abc12345', // The optional customer id. Generates an additional field for storing a customer identifier in cases where an app is used across multiple Apigrate customers/subscribers.
+    author_url: "https://www.example.com/myapp/readme",
+    fields: {
+      "account": "abc123",
+      "apigrate_account": 107
+    }
   }
 
-  For the options parameter, there are two supported options:
-  1. "solution_url" This is an optional string representing the solution URL link. Could be README documentation, for example for an app.
-  2. "customer_id" This is optional customer id. It results in  an additional attachment field for storing a customer identifier in cases where an app (i.e. the solution)
-  is used across multiple Apigrate customers/subscribers.
-
-  @version 2.0.2
+  @version 3.0.0
 
 */
-function SlackLogger(inbound_webhook, hostname, solution, options) {
-  if( _.isNil(inbound_webhook) || _.isNil(hostname) || _.isNil(solution) ){
-    throw new Error("Misconfigured SlackLogger. The inbound_webhook, hostname, and solution parameters are all required.");
+class SlackLogger{
+  constructor(inbound_webhook, username, author, options){
+    if( _.isNil(inbound_webhook) || _.isNil(username) || _.isNil(author) ){
+      throw new Error("Misconfigured Slack Logger. The inbound_webhook, username, and author parameters are all required.");
+    }
+    this.inbound_webhook = inbound_webhook;
+    this.username = username;
+    this.author = author;
+    this.options = options;
+    if (!options) {
+      this.options = {};
+    }
   }
-  this.inbound_webhook = inbound_webhook;
-  this.hostname = hostname;
-  this.solution = solution;
-  this.options = options;
-  if (!options) {
-    this.options = {};
+
+  /**
+    Log a slack logging message.
+
+    @param {boolean} success required true/false
+    @param {string} summary required a short summary message (i.e. "synced ok", "error processing account", etc.)
+    @param {string} details optional details to be displayed in fixed-width block under the message. Use this to output transcript info.
+    Up to 7500 characters will be written, after which the data will be truncated.
+    @param {object} an noptional hash of properties that will be added as short fields on this particular slack entry.
+  */
+  log(success, summary, details, fields) {
+    var self = this;
+
+    if( _.isNil(success) || _.isNil(summary) ){
+      throw new Error("Invalid log invocation. The success, entity, entity_id, and summary parameters are all required.");
+    }
+
+    var color = "good";
+    var emoji = ":white_check_mark:";
+    if (success) {
+      color = "good";
+      emoji = ":white_check_mark:";
+    } else {
+      color = "danger";
+      emoji = ":x:";
+    }
+
+    var title = emoji + ' ' + summary;
+
+    var text = "";
+    if (details) {
+      if (details.length > 7500) {
+        text = details.substr(0, 7500) + "...";
+      } else {
+        text = details;
+      }
+    }
+
+    if (text && text.trim().length > 0) {
+      text = "```" + text + "```";
+    }
+
+    var slack_message = {
+      "username": self.username,
+      "attachments": [
+        {
+          "color": color,
+          "author_name": self.author || "",
+          "author_link": self.options.author_url || "",
+          "title": title,
+          "text": text,
+          "ts": moment().unix(),
+          "mrkdwn_in": ["pretext","text"],
+          "fields": [
+            {
+              "title": "author",
+              "value": self.author,
+              "short": true
+            }, {
+              "title": "success",
+              "value": success
+                ? "true"
+                : "false",
+              "short": true
+            }
+
+          ]
+        }
+      ]
+    };
+
+    //global fields
+    if(self.options && self.options.fields){
+      _.each(self.options.fields, function(val, name){
+        slack_message.attachments[0].fields.push({
+          title: name,
+          value: val,
+          short: true
+        });
+      })
+    }
+
+    //specific fields
+    if(fields){
+      _.each(fields, function(val, name){
+        slack_message.attachments[0].fields.push({
+          title: name,
+          value: val,
+          short: true
+        });
+      })
+    }
+
+    if(self.options.customer_id){
+      var custField = {
+        "title": "customer id",
+        "value": self.options.customer_id,
+        "short": true
+      };
+      slack_message.attachments[0].fields.push(custField);
+    }
+
+    return request({
+      method: "POST",
+      url: self.inbound_webhook,
+      json: true,
+      body: slack_message
+    })
+    .then(function(result){
+      return Promise.resolve( {success: true} );
+    })
+    .catch(function(err){
+    	return Promise.reject( new Error(`Error. Slack responded with:\n${body}`) );
+    });
   }
 }
-
-/**
-  Log a summary message.
-
-  @param {boolean} success required true/false
-  @param {string} entity required type of the primary entity for the solution ("what kind of thing is being worked on")
-  @param {string} entity_id required identifier for the primary entity.
-  @param {string} summary required a short summary message (i.e. "synced ok", "error processing account", etc.)
-  @param {string} details optional details to be displayed in fixed-width block under the message. Use this to output transcript info.
-  Up to 7500 characters will be written, after which the data will be truncated.
-*/
-SlackLogger.prototype.log = function(success, entity, entity_id, summary, details) {
-  var self = this;
-
-  if( _.isNil(success) || _.isNil(entity) || _.isNil(entity_id) || _.isNil(summary) ){
-    throw new Error("Invalid log invocation. The success, entity, entity_id, and summary parameters are all required.");
-  }
-
-  var color = "good";
-  var emoji = ":white_check_mark:";
-  if (success) {
-    color = "good";
-    emoji = ":white_check_mark:";
-  } else {
-    color = "danger";
-    emoji = ":x:";
-  }
-
-  var title = emoji + ' ' + summary;
-
-  var text = "";
-  if (details) {
-    if (details.length > 7500) {
-      text = details.substr(0, 7500) + "...";
-    } else {
-      text = details;
-    }
-  }
-
-  if (text && text.trim().length > 0) {
-    text = "```" + text + "```";
-  }
-
-  var slack_message = {
-    "username": self.hostname,
-    "attachments": [
-      {
-        "color": color,
-        "author_name": self.solution || "",
-        "author_link": self.options.solution_url || "",
-        "title": title,
-        "text": text,
-        "ts": moment().unix(),
-        "mrkdwn_in": ["text"],
-        "fields": [
-          {
-            "title": "solution",
-            "value": self.solution,
-            "short": true
-          }, {
-            "title": "success",
-            "value": success
-              ? "true"
-              : "false",
-            "short": true
-          }, {
-            "title": "entity",
-            "value": entity,
-            "short": true
-          }, {
-            "title": "entity id",
-            "value": entity_id,
-            "short": true
-          }
-
-        ]
-      }
-    ]
-  };
-
-  if(self.options.customer_id){
-    var custField = {
-      "title": "customer id",
-      "value": self.options.customer_id,
-      "short": true
-    };
-    slack_message.attachments[0].fields.push(custField);
-  }
-
-  return request({
-    method: "POST",
-    url: self.inbound_webhook,
-    json: true,
-    body: slack_message
-  }, function(err, resp, body) {
-    if (err) {
-      throw err;
-    } else {
-      if (body == 'ok') {
-        return {success: true};
-      } else {
-        throw new Error("Error. Slack responded with: " + body);
-      }
-    }
-
-  });
-};
 
 module.exports=SlackLogger;
